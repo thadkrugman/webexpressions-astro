e532783c6a218ff756bda3ec9abb5f91ef30e72b
Thad Krugman <thaddeuskrugman@gmail.com>
2025-06-05 19:10:01 -0400

    init commit

---
+++ src/constants/nav-items.ts
@@ -0,0 +1,22 @@
+export const NAV_ITEMS = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'About',
+    href: '/about',
+  },
+  {
+    label: 'Portfolio',
+    href: '/portfolio',
+  },
+  {
+    label: 'Blog',
+    href: '/blog',
+  },
+  {
+    label: 'Contact',
+    href: '/contact',
+  },
+];
